60b9f062bb4a12e744bada76abe2893b9bb3bc25
alexey114 <bomkoar@gmail.com>
2021-11-19 16:12:00 +0300

    add files field + css

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Field from './Field';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Рисуй линии!</h1>
+      <Field />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Field.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Field.css';
+import './Action';
+
+function Field() {
+  return (
+
+      <div className="Field">
+          <div className="Field_items">
+            <div className="Field_item">
+
+            </div>
+          </div>
+      </div>
+  );
+}
+
+export default Field;
