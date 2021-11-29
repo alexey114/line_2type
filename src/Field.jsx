95b24366a0e3bb6654d95ae5392bbf7aad2336cd
alexey114 <bomkoar@gmail.com>
2021-11-29 04:38:46 +0300

    add line drawing on click

--- src/Field.jsx
+++ src/Field.jsx
@@ -7,28 +7,38 @@ class Field extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {onClick: false}; //текущее состояние
+    this.state = { arrayClick: [] }; //текущее состояние
 
     // Эта привязка обязательна для работы `this` в колбэке.
-    this.handleClick = this.handleClick.bind(this);
+    this.arrClon = this.arrClon.bind(this);
   }
 
-  handleClick($event) {
-    this.setState(console.log($event.clientX, $event.clientY)); //новое состояние при клике вызывает консоль лог с координатами
-  }
+  arrClon (event) {
+
+    if(this.state.arrayClick < 3){
+      let arrClonAdd = this.state.arrayClick.concat(`M${event.clientX} ${event.clientY}`);
+      this.setState({arrayClick:arrClonAdd});
+    } else {
+      let arrClonT = this.state.arrayClick.concat(`L${event.clientX} ${event.clientY}`);
+      console.log(event.clientX, event.clientY);
+      this.setState({arrayClick:arrClonT});
+    };
+      }
 
   render() {
   return (
-          <div className="Field_item" onClick={this.handleClick}>
-                <svg id="svg_field" width="350" height="300" viewBox="0 0 350 300" xmlns="http://www.w3.org/2000/svg">
-                      <path id="line" d="M10 250 L250 100" stroke="red"/>
+
+          <div className="Field_item">
+                <svg onClick={this.arrClon} id="svg_field" width="350" height="300" viewBox="0 0 350 300" xmlns="http://www.w3.org/2000/svg">
+
+                      <path id="line" d={this.state.arrayClick.join(" ")} stroke="red"/>
+
                 </svg>
+                <h1>{this.state.arrayClick}</h1>
           </div>
 );
 }
 }
-
- 
 
 function Ok() {
   console.log('Ok');
