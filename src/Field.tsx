71fd3685efc552f69fd85bcbe11dafc6659706a6
alexey114 <bomkoar@gmail.com>
2021-11-30 23:28:14 +0300

    add localstorage save

--- src/Field.tsx
+++ src/Field.tsx
@@ -2,12 +2,6 @@ import React from 'react';
 import './Field.css';
 import './Button.css';
 
-
-
-export interface IButtonProps {
-  button: boolean
-}
-
 interface IFieldProps {
 }
 
@@ -16,7 +10,8 @@ export interface IFieldState {
   addCordinateArray: (number | string)[],
   color: string,
   buttonRed: string,
-  buttonSave: string
+  buttonSave: string,
+  buttonZ: string
 }
 
 export class Field extends React.Component<IFieldProps, IFieldState> {
@@ -28,7 +23,8 @@ export class Field extends React.Component<IFieldProps, IFieldState> {
       addCordinateArray: [],
       color: "black",
       buttonRed: "Красный выкл",
-      buttonSave: "Сохранить"
+      buttonSave: "Сохранить",
+      buttonZ: "Закрытие линий выкл"
     }; //текущее состояние
 
     // Эта привязка обязательна для работы `this` в колбэке.
@@ -37,6 +33,8 @@ export class Field extends React.Component<IFieldProps, IFieldState> {
     this.textChangeSave = this.textChangeSave.bind(this);
     this.colorChange = this.colorChange.bind(this);
     this.addCordinateArrayAll = this.addCordinateArrayAll.bind(this);
+    this.coordinateSave = this.coordinateSave.bind(this);
+    this.completeFigureButton = this.completeFigureButton.bind(this);
   }
 
   //   toggleButtonRed = () => {
@@ -45,17 +43,44 @@ export class Field extends React.Component<IFieldProps, IFieldState> {
   //         setColor("red")
   //     }
   // }
+
   textChange() {
     this.setState({ buttonRed: "Красный вкл" });
-    this.colorChange();
   }
 
   colorChange() {
     this.setState({ color: "red" });
+    this.textChange()
   }
 
   textChangeSave() {
     this.setState({ buttonSave: "Сохранено" });
+  }
+
+  // ________________________________________Local Storage_______________________________________  //
+
+  coordinateSave() {
+    localStorage.setItem("arrayCoordinate", JSON.stringify(this.state.arrayCoordinate));
+    let localStorageCoordinate = localStorage.getItem("arrayCoordinate");
+    console.log("localStorageCoordinate:", localStorageCoordinate);
+
+    this.textChangeSave()
+  }
+
+
+  // ________________________________________Local Storage END_______________________________________  //
+
+  completeFigureText () {
+      this.setState({ buttonZ: "Закрытие линий вкл" });
+  }
+
+  completeFigureButton() {
+
+    if (this.state.arrayCoordinate.length > 2) {
+      this.completeFigureText ();
+      let arrClonAdd = this.state.arrayCoordinate.concat("Z");
+      this.setState({ arrayCoordinate: arrClonAdd });
+    }
   }
 
   addCordinateArrayAll(event: any) {
@@ -63,7 +88,7 @@ export class Field extends React.Component<IFieldProps, IFieldState> {
     this.setState({ addCordinateArray: arrClonAddAll });
     console.table(arrClonAddAll);
   }
-  
+
   // componentDidUpdate (prevState) {
   //   if (this.state.color === prevState.color) {
   //     this.setState(this.state.color);
@@ -76,8 +101,8 @@ export class Field extends React.Component<IFieldProps, IFieldState> {
       let arrClonAdd = this.state.arrayCoordinate.concat(`M${event.clientX} ${event.clientY}`);
       this.setState({ arrayCoordinate: arrClonAdd });
     } else {
-      let arrClonL = this.state.arrayCoordinate.concat(`L${event.clientX} ${event.clientY}`);
-      this.setState({ arrayCoordinate: arrClonL });
+      let arrClonAdd = this.state.arrayCoordinate.concat(`L${event.clientX} ${event.clientY}`);
+      this.setState({ arrayCoordinate: arrClonAdd });
     };
   }
 
@@ -88,13 +113,13 @@ export class Field extends React.Component<IFieldProps, IFieldState> {
         <svg onClick={this.arrClon} id="svg_field" width="350" height="300" viewBox="0 0 350 300" xmlns="http://www.w3.org/2000/svg">
           <path id="line" d={this.state.arrayCoordinate.join(" ")} stroke={this.state.color} />
         </svg>
-        {/* <button className="button_z" onClick={toggleButtonZ}>{buttonZ ? 'Закрытие линий вкл' : 'Закрытие линий выкл'}</button> */}
-        <h1>{this.state.buttonRed}</h1>
-        <button className="color_black" onClick={this.textChange}>{this.state.buttonRed} </button>
-        <button className="save" onClick={this.textChangeSave}>{this.state.buttonSave} </button>
+
+        <button className="button_z" onClick={this.completeFigureButton}>{this.state.buttonZ}</button>
+        <button className="color_black" onClick={this.colorChange}>{this.state.buttonRed} </button>
+        <button className="save" onClick={this.coordinateSave}>{this.state.buttonSave} </button>
 
 
-      </div> //(a:React.MouseEvent|React.TouchEvent) => {console.log(1)}
+      </div>
     );
   }
 }
